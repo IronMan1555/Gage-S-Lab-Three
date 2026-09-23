313fbec37de5aa1e8ad1c739307c4e3a220f3890
Gage Sanders <gsanders29@setoncatholic.org>
2026-09-23 20:07:51 +0000

    Version 1

---
+++ main.py
@@ -0,0 +1,28 @@
+#function that adds two numbers together
+def add(x,y):
+    print(x+y)
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+add(x,y)
+
+#function that subtracts two numbers
+def sub(x,y):
+    print(x-y)
+
+sub(x,y)
+
+#multiply two numbers together
+def mult(x,y):
+    print(x*y)
+
+mult(8,9)
+
+
+
+#divide two numbers
+def div(x,y):
+    print(5/5)
+
+div(x,y)
